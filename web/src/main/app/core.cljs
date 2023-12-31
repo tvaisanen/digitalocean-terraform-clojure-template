6204d26c4c9a863335968c1cb5872685c5ff8f20
Toni Väisänen <vaisanen.toni@gmail.com>
2023-12-31 11:12:50 +0200

    spike frontend

---
+++ web/src/main/app/core.cljs
@@ -0,0 +1,23 @@
+(ns app.core
+  (:require [app.utils :as utils]
+            [clojure.pprint :refer [pprint]]
+            [clojure.edn :as edn]
+            [helix.core :refer [defnc]]
+            [helix.dom :as d]
+            [helix.hooks :as hooks]))
+
+(defnc App []
+  (let [[data set-data] (hooks/use-state {})]
+    (hooks/use-effect
+     :once
+     (-> (js/fetch "/api/whatever")
+         (.then (fn [res] (. res text)))
+         (.then (fn [response-string]
+                  (set-data (edn/read-string response-string))))))
+    (d/div
+     (d/div "App Here")
+     (d/pre (with-out-str
+              (pprint data))))))
+
+(defn init []
+  (utils/render App))
